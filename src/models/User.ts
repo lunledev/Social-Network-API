f32a0093c09c62af25fd943be9245e62b9a7948e
lunledev <183235731+lunledev@users.noreply.github.com>
2025-02-21 14:42:02 -0800

    initial commit

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,21 @@
-//import schema and dodel from mngoose
+//import schema and model from mongoose
+import { Schema, model, Document } from "mongoose";
+
 
 //inteface
+interface IUser extends Document{
+username: string;
+email: string;
 
-
+}
 //construct a new instance of the schema class
+const userSchema = new Schema<IUser>(
+    {
+        username: {type: String, required: true },
+        email: {}.
+
+    }
+);
 
 // using model() to compile a  model based on schema userSchema
 
