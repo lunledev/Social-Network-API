62d9b41d4aea3f62d890aa8c0240e234d91d6d7d
lunledev <183235731+lunledev@users.noreply.github.com>
2025-02-27 17:32:09 -0800

    added reactionId

--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -1,7 +1,18 @@
 import { Schema, Document, ObjectId, Types } from "mongoose";
 
+interface IReaction extends Document{
 
+    reactionId: ObjectId;
+}
 
+const reactionSchema = new Schema<IReaction>(
+{
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+
+});
 
 
 
