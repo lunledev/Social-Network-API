5fd01b8cd22167aa2bee440a887a9fb9c4ebfc41
lunledev <183235731+lunledev@users.noreply.github.com>
2025-03-03 01:55:36 -0800

    updates to userController.ts api/index.ts api/userRoutes.ts routes/index.ts, server.ts

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,2 +1,13 @@
 //path for the routes.
+import { Router } from "express";
+
+const router = Router();
+
+import userRoutes from './userRoutes.js';
+
+router.use('/users',userRoutes);
+
+export default router;
+
+
 
